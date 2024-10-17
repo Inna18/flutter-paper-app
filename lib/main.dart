236b4aacf950835a0fe791176957c6690f6991f0
Inna18 <kinvdev21@gmail.com>
2024-10-17 09:37:37 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:paper_app/screens/traces.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '스마트팟 고객사 전용 페이지',
+      theme: ThemeData(
+        colorScheme:
+            ColorScheme.fromSeed(seedColor: Color.fromRGBO(1, 20, 57, 1)),
+        useMaterial3: true,
+      ),
+      home: const TracesScreen(),
+    );
+  }
+}
---
+++ lib/screens/traces.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:paper_app/widgets/traces_list.dart';
+
+class TracesScreen extends StatefulWidget {
+  const TracesScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _TracesScreenState();
+  }
+}
+
+class _TracesScreenState extends State<TracesScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('배송조회'),
+          backgroundColor: const Color.fromRGBO(1, 20, 57, 1),
+          foregroundColor: const Color.fromRGBO(255, 255, 255, 1),
+        ),
+        body: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Expanded(
+              child: Container(
+                decoration:
+                    const BoxDecoration(color: Color.fromRGBO(1, 20, 57, 1)),
+                child: Positioned(
+                  top: 0,
+                  bottom: 0,
+                  left: 0,
+                  right: 0,
+                  child: Expanded(
+                    child: Container(
+                      decoration: const BoxDecoration(
+                          color: Color.fromRGBO(255, 255, 255, 1),
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(24),
+                              topRight: Radius.circular(24))),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ));
+  }
+}
